67486b014d17a52a65574d0da1c18a1418978413
Yi Zhao <yz8pd@mail.missouri.edu>
2018-07-14 01:01:32 +0800

    set background colors

---
+++ colors/colors/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  colors
+//
+//  Created by 赵益 on 7/14/18.
+//  Copyright © 2018 Yi Zhao. All rights reserved.
+//
+
+import UIKit
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
